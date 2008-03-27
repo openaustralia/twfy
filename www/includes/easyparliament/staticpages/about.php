45adc6c223258d2317a16f09431a612df331339e
Matthew Landauer <matthew@openaustralia.org>
2008-03-27 20:10:32 +1100

    Updated about page with more relevant content for OpenAustralia

--- www/includes/easyparliament/staticpages/about.php
+++ www/includes/easyparliament/staticpages/about.php
@@ -1,6 +1,6 @@
 <p>For all its faults and foibles, our democracy is a profound gift from previous generations.
-Yet most people don't know the name of their MP, nor their constituency, let alone what their
-MP does or says in their name.</p>
+Yet most people don't know the name of their representative, let alone what they
+do or say in their name.</p>
 
 <p>We aim to help bridge this growing democratic disconnect, in the belief that there is
 little wrong with Parliament that a healthy mixture of transparency and public engagement won't fix.</p>
@@ -9,60 +9,37 @@ little wrong with Parliament that a healthy mixture of transparency and public e
 
 <h3>Who runs this site?</h3>
 
-<p>OpenAustralia is a website run by <a href="http://www.mysociety.org/">mySociety</a>,
-which is itself a project of UK Citizens Online Democracy, a registered charity.
-It was originally built almost entirely by volunteers (see <a href="#history">History</a> below),
-but now mySociety pays Matthew to keep the site running and up-to-date as part of his
-wider work for mySociety. However, things are still added voluntarily by anyone who wishes.
+<p>OpenAustralia is a website entirely started and run by a group of volunteers.
+We owe a great debt to a UK charity, mySociety, which with the help of a group of
+volunteers built <a href="http://theyworkforyou.com/">theyworkforyou.com</a> which this site is based on.
 </p>
 
-<p>The copyright of Hansard remains under
-<a href="http://www.opsi.gov.uk/advice/parliamentary-copyright/">Parliamentary Copyright</a>,
-used under licence.
+<p>The Australian Hansard is
+<a href="http://www.ag.gov.au/cca">Copyright Commonwealth of Australia</a>,
+and is used with permission.
 
 <p>If you're technically minded, we make all the
-<a href="https://secure.mysociety.org/cvstrac/dir?d=mysociety/twfy">OpenAustralia source code</a>
+<a href="http://software.openaustralia.org/">OpenAustralia source code</a>
 freely available. Do let us know if you want to help!
 </p>
 
 
 <h3><a name="history"></a>History</h3>
 
-<p>OpenAustralia was set up almost entirely <a href="#1">[1]</a> by a dozen or so volunteers
-who thought it should be really easy for people to keep tabs on their elected MPs,
-and their unelected Peers, and comment on what goes on in Parliament. They'd done this
-sort of thing before, but never on this scale.</p>
+<p>CONTENT TO BE ADDED</p>
 
-<p>(<a name="1">[1]</a> One of the key developers was kept fed for a month by a small grant
-from UK Citizen's Online Democracy, before OpenAustralia became a mySociety project. This
-money was vital. Hungry developers don't write good code. If you like
-OpenAustralia, and want to support mySociety, you can <a
-href="https://secure.mysociety.org/donate/">make a donation</a> yourself.)</p>
+<h4>Volunteers on OpenAustralia.org</h4>
+<ul>
+	<li>Matthew Landauer</li>
+	<li>Katherine Szuminska</li>
+	<li>Bruno Mattarollo</li>
+	<li>Matthew Panetta</li>
+</ul>
 
-<p>Between them, the original volunteers had developed and run a range of not-for-profit websites of a vaguely similar ilk.
-<a href="http://www.publicwhip.org.uk/">PublicWhip.org.uk</a>,
-<a href="http://www.pepysdiary.com/">PepysDiary.com</a>,
-<a href="http://www.haddock.org/">Haddock.org</a>,
-<a href="http://www.byliner.com/">Byliner.com</a>,
-<a href="http://www.b3ta.com/">B3Ta</a>,
-<a href="http://www.downingstreetsays.com/">DowningStreetSays.com</a>,
-<a href="http://www.mysociety.org/">MySociety</a>,  and an accessible re-versioning of the
-<a href="http://www.traintimes.org.uk/">National Rail Timetable</a> website.
-They conceived and developed the original <a href="http://www.upmystreet.com/">UpMyStreet.com</a>
-back in <a href="http://web.archive.org/web/19981202165125/http://www.upmystreet.com/">1998</a>. 
-</p>
-
-<p>In early 2006 Tom and Stef decided that the overlap of people and goals with mySociety
-was so substantial that it was best to pass the running over to mySociety, who are an actual
-proper organisation with staff and time.</p>
-
-<p>Matthew added the Northern Ireland Assembly as an early Christmas present in 2006, working
-alone and voluntarily, perhaps as a nudge to the Scots and the Welsh to come forward and 
-work on their own versions. :)</p>
-
-<h4>Volunteers</h4>
 <p><em>They did all this in their spare time, because they thought it was worth doing.
 And if it's worth doing, you might as well try to do it well. </em></p>
+
+<h4>Volunteers on TheyWorkForYou.com</h4>
 
 <ul>
 <li>Richard Allan</li>
