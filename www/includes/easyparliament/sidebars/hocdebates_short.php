28d5724c5188626c700c2e98af29641ad449fc71
Matthew Landauer <matthew@openaustralia.org>
2008-05-02 22:41:49 +1000

    Hopefully fixed up link in hansard "What are House debates?"

--- www/includes/easyparliament/sidebars/hocdebates_short.php
+++ www/includes/easyparliament/sidebars/hocdebates_short.php
@@ -1,10 +1,13 @@
 <?php
 // This sidebar is on the pages that show all the text of a particular debate.
 
+$URL = new URL('debates');
+$debatesurl = $URL->generate();
+
 $this->block_start(array(
 	'id'=>'help', 
 	'title'=>"What are House debates?", 
-	'url'=>'/debates/#help',
+	'url'=> $debatesurl . '#help',
 	'body'=>false
 ));
 $this->block_end();
