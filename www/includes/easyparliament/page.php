63a800382355ee8f41f6ffabb3f7bc531cfe5b97
Matthew Landauer <matthew@openaustralia.org>
2008-05-02 19:19:47 +1000

    Fixed bug where link on members page for the number of times they spoke in a debate was linking to a search page for more houses than just the house of representatives.

--- www/includes/easyparliament/page.php
+++ www/includes/easyparliament/page.php
@@ -1498,7 +1498,8 @@ and has had no written questions answered for which we know the department or su
 		#	$since_text = 'since joining Parliament';
 
 		$MOREURL = new URL('search');
-		$section = 'section:debates section:whall section:lordsdebates section:ni';
+		#$section = 'section:debates section:whall section:lordsdebates section:ni';
+		$section = 'section:debates';
 		$MOREURL->insert(array('pid'=>$member['person_id'], 's'=>$section, 'pop'=>1));
 		if ($member['party']!='Sinn Fein') {
 			$displayed_stuff |= display_stats_line('debate_sectionsspoken_inlastyear', 'Has spoken in <a href="' . $MOREURL->generate() . '">', 'debate', '</a> ' . $since_text, '', $extra_info);
