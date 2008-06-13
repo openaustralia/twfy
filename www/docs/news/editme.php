24d7c66107742755fd0bc0df4241d1c18cb8aa82
Matthew Landauer <matthew@openaustralia.org>
2008-06-13 10:56:51 +1000

    Updated news for public launch

--- www/docs/news/editme.php
+++ www/docs/news/editme.php
@@ -4,17 +4,31 @@
 
 $all_news = array(
 
-1 => array('Birth of OpenAustralia', <<<EOT
-By hacking, levering, doing terrible things while nobody is looking,
-pummeling and other things in that general direction, we've managed
-to somehow get a very crude version of
-OpenAustralia up and running. All credit goes to the UK guys behind
-TheyWorkForYou.com for
-putting so much love and labour into a codebase that made this all
-really surprisingly straightforward. Thank you!
-EOT
-, '2007-11-22 17:46:00', 'Matthew'),
+1 => array('Public Launch of OpenAustralia Beta', <<<EOT
+We're open to the public! After more than six months work by a small
+group of volunteers working in their spare time, we're launching
+OpenAustralia to the public. We hope that this site will become an
+important part of ensuring a continued and improved fair, democratic
+and open Australian Parliamentary system.
 
+This site is about giving you, the Australian public, the tools to easily
+follow what goes on in Parliament and be just a little better informed.
+
+You'll hopefully discover with OpenAustralia that what goes on in Parliament
+is much more interesting than you might imagine - entertaining even!
+
+What you see on TV or in newspapers is only a small part of what goes on.
+OpenAustralia is about giving you access to all the other stuff in an easy
+to use and friendly form.
+
+This site is still in beta, which means that we're not totally confident that
+we've ironed out all the small problems, but we didn't think it fair to keep
+it away from you because of how useful it is. If you do find something
+that looks wrong please email matthew at openaustralia dot org.
+
+Enjoy!
+EOT
+, '2008-6-16 9:00:00', 'Matthew'),
 
 );
 
