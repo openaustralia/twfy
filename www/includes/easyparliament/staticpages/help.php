f2dd2271d79576a7aaf8b0e61e552cdf7dacc65d
Matthew Landauer <matthew@openaustralia.org>
2008-06-21 16:54:38 +1000

    Fixing link to aph.gov.au site

--- www/includes/easyparliament/staticpages/help.php
+++ www/includes/easyparliament/staticpages/help.php
@@ -27,7 +27,7 @@
 	<p>Not quite. This is everything in the Hansard for the House of Representatives excluding written questions, petitions,
 		the divisions (voting), and procedural text and so far goes back to the beginning of 2006. It also does not include committees. Think of what we've done
 thus far as a mere taster of what could be possible. In the future we plan to add voting information, the Senate and committees too.
-If you want the complete, definitive record, go to the <a href="http://aph.gov.au/" title="Link to Australian Parliament website">Australian Parliament</a> site, and you might be able to find what you want.</p>
+If you want the complete, definitive record, go to the <a href="http://www.aph.gov.au/" title="Link to Australian Parliament website">Australian Parliament</a> site, and you might be able to find what you want.</p>
 </dd>
 <!-- end old faq entry -->
 
