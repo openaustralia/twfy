2865fb16b8b189535f8067702e79eeef1e1620c1
Matthew Landauer <matthew@openaustralia.org>
2008-08-18 15:55:59 +1000

    Fix to make the HTML properly validating again

--- www/includes/easyparliament/page.php
+++ www/includes/easyparliament/page.php
@@ -214,7 +214,7 @@ class PAGE {
 	<title><?php echo $title; ?></title>
 	<meta name="description" content="Making parliament easy.">
 	<meta name="keywords" content="Parliament, government, House of Representatives, Senate, Senator, MP, Member of Parliament, MPs, Australia, Australian, <?php echo htmlentities($keywords_title).htmlentities($keywords); ?>">
-	<meta name="verify-v1" content="5FBaCDi8kCKdo4s64NEdB5EOJDNc310SwcLLYHmEbgg=" />
+	<meta name="verify-v1" content="5FBaCDi8kCKdo4s64NEdB5EOJDNc310SwcLLYHmEbgg=">
 	<link rel="author" title="Send feedback" href="mailto:<?php echo str_replace('@', '&#64;', CONTACTEMAIL); ?>">
 	<link rel="home" title="Home" href="http://<?php echo DOMAIN; ?>/">
 <?php
