2b033a1cfcdfbf94604c84be29412db424f43732
Matthew Landauer <matthew@openaustralia.org>
2008-08-26 16:16:36 +1000

    Added urls for Hansard

--- scripts/sitemap.rb
+++ scripts/sitemap.rb
@@ -1,17 +1,21 @@
 #!/usr/bin/env ruby
 # Generate sitemap.xml for quick and easy search engine updating
 
-# Useful tables:
-#   hansard
-#   epobject
-#   member
-
 require 'rubygems'
 require 'active_record'
 require "../../rblib/config"
 
 # Load database information from the mysociety configuration
 MySociety::Config.set_file("../conf/general")
+
+# Establish the connection to the database
+ActiveRecord::Base.establish_connection(
+	:adapter  => "mysql",
+	:host     => MySociety::Config.get('DB_HOST'),
+	:username => MySociety::Config.get('DB_USER'),
+	:password => MySociety::Config.get('DB_PASSWORD'),
+	:database => MySociety::Config.get('DB_NAME')
+)
 
 class Member < ActiveRecord::Base
 	set_table_name "member"
@@ -32,7 +36,6 @@ class Member < ActiveRecord::Base
 	# Returns the unique url for this member.
 	# Obviously this doesn't really belong in the model but, you know, for the time being...
 	def url
-		domain = "http://www.openaustralia.org"
 		if house == 1
 			house_url = "mp"
 		elsif house == 2
@@ -42,31 +45,34 @@ class Member < ActiveRecord::Base
 		end
 		# The url is made up of the full_name, constituency and house
 		# TODO: Need to correctly encode the urls
-		domain + "/" + house_url + "/" + full_name.downcase.tr(' ', '_') + '/' + constituency.downcase
+		"http://" + MySociety::Config.get('DOMAIN') + "/" + house_url + "/" + full_name.downcase.tr(' ', '_') + '/' + constituency.downcase
 	end
 end
 
-#ActiveRecord::Base.primary_key_prefix_type = :table_name_with_underscore
+class Hansard < ActiveRecord::Base
+	set_table_name "hansard"
+	
+	# Returns the unique url for this bit of the Hansard
+	# Again, this should not really be in the model
+	def url
+		if gid =~ /^uk.org.publicwhip\/(lords|debate)\/(.*)$/
+			house = ($~[1] == "debate") ? "debate" : "senate"
+			"http://" + MySociety::Config.get('DOMAIN') + "/" + house + "/?id=" + $~[2]
+		else
+			throw "Unexpected form of gid #{gid}"
+		end
+	end
+end
 
-ActiveRecord::Base.establish_connection(
-	:adapter  => "mysql",
-	:host     => MySociety::Config.get('DB_HOST'),
-	:username => MySociety::Config.get('DB_USER'),
-	:password => MySociety::Config.get('DB_PASSWORD'),
-	:database => MySociety::Config.get('DB_NAME')
-)
-
-member = Member.find_most_recent_by_person_id(10093)
-
-p member
-
-#member_urls = Member.find(:all).map {|member| member.url}.uniq.sort
-#member_urls.each do |url|
-#	puts url
-#end
-
+# All the member urls (Representatives and Senators)
 urls = Member.find_all_person_ids.map {|person_id| Member.find_most_recent_by_person_id(person_id).url}
+# All the Hansard urls (for both House of Representatives and the Senate)
+urls = urls + Hansard.find(:all).map {|h| h.url}
 
 urls.each do |url|
 	p url
 end
+
+puts "There were #{urls.size} urls in the sitemap"
+
+
