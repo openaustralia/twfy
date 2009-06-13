6d056e115d9d96004daeb315f6891b01ff92c818
Ray Haleblian <ray@haleblian.com>
2009-06-13 14:48:03 +1000

    Resolution for OA-220.

--- www/includes/easyparliament/sidebars/mps.php
+++ www/includes/easyparliament/sidebars/mps.php
@@ -9,7 +9,7 @@ $this->block_start(array('title'=>"This data as a spreadsheet",
 	'body'=>''));
 ?>
 <p>
-	<a href="<?= $csvurl ?>">Download a CSV</a> (Comma Separated Values) file that you can load into Excel.
+	<a href="<?= $csvurl ?>">Download a CSV</a> (Comma Separated Values) file that you can load into your favourite spreadsheet program or data-mashing software.
 </p>
 <?php
 $this->block_end();
