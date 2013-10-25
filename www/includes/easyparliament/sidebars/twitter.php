4376530f52d44903ec72abdee936e13de5b3edb5
Henare Degan <henare.degan@gmail.com>
2013-10-25 22:52:52 +0200

    Update the Twitter widget

--- www/includes/easyparliament/sidebars/twitter.php
+++ www/includes/easyparliament/sidebars/twitter.php
@@ -1,34 +1,8 @@
 <?php
 $this->block_start(array('id'=>"twitter"));
 ?>
-<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: 30000,
-  width: 'auto',
-  height: 300,
-  theme: {
-    shell: {
-      background: '#a8a98c',
-      color: '#ffffff'
-    },
-    tweets: {
-      background: '#ffffff',
-      color: '#000000',
-      links: '#0031e2'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: false,
-    live: false,
-    behavior: 'all'
-  }
-}).render().setUser('OpenAustralia').start();
-</script>
+<a class="twitter-timeline"  href="https://twitter.com/OpenAustralia"  data-widget-id="349036160594632704">Tweets by @OpenAustralia</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <?php
 $this->block_end();
 ?>
