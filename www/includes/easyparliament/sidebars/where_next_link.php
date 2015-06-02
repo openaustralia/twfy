21fff0c18f69e3f04179a9eb6ab9f60c3b6dab28
Luke Bacon <luke@equivalentideas.com>
2015-06-02 22:00:48 +1000

    Add sidebar widget for link to 'Where next' page
    relates to https://github.com/openaustralia/openaustralia/issues/572

---
+++ www/includes/easyparliament/sidebars/where_next_link.php
@@ -0,0 +1,6 @@
+<?php // sidebar link for mySociety experiment ?>
+<div hidden id="research-quant2-bucket2" class="block">
+  <h4>
+    <a href="/wherenext">What to do with the information on this page?</a>
+  </h4>
+</div>
