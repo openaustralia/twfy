f7bdc5467880e4594074fa1245d55507b2c56f78
Luke Bacon <luke@equivalentideas.com>
2015-09-01 09:03:31 +1000

    Add template file for link to survey

---
+++ www/includes/easyparliament/templates/research/demographic-survey-link.php
@@ -0,0 +1,7 @@
+<div id="ms_srv_wrapper" class="pre-header-notice" style="/* display:none */">
+  <p>
+    <a href="https://www.surveygizmo.co.uk/s3/2262048/2014-Baselining-OpenAustralia" id="ms_srv_link" data-transaction="report">
+      We're running a survey to help make this site better; to take it click here.
+    </a>
+  </p>
+</div>
