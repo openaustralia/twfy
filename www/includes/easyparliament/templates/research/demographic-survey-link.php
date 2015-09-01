e2ff6885c8e6669eabbd9d00330c92ddf6766e61
Luke Bacon <luke@equivalentideas.com>
2015-09-01 12:31:10 +1000

    Don't show the survey link unless the script activates it
    This should only get shown to 30% of visitors. The
    demographic-survey.min.js script removes the display declaration,
    revealing the link.

--- www/includes/easyparliament/templates/research/demographic-survey-link.php
+++ www/includes/easyparliament/templates/research/demographic-survey-link.php
@@ -1,4 +1,4 @@
-<div id="ms_srv_wrapper" class="pre-header-notice" style="/* display:none */">
+<div id="ms_srv_wrapper" class="pre-header-notice" style="display:none">
   <a href="https://www.surveygizmo.co.uk/s3/2262048/2014-Baselining-OpenAustralia" id="ms_srv_link" data-transaction="report">
     Help improve OpenAustralia by answering a few quick questions
   </a>
