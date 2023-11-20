3b41c6a18aba55d801bb91c9dac4fb4b751edec0
Matthew Landauer <mlandauer@gmail.com>
2023-11-20 11:33:22 +1100

    Link to correct OAF donation page

--- www/includes/easyparliament/sidebars/whatisthissite.php
+++ www/includes/easyparliament/sidebars/whatisthissite.php
@@ -10,8 +10,7 @@ $URL = new URL('help');
 $helpurl = $URL->generate();
 ?>
 
-<p><a href="https://www.openaustraliafoundation.org.au/donate/openaustralia-org/"
-        onClick="javascript:pageTracker._trackPageview('/outgoing/www.openaustraliafoundation.org.au/donate/openaustralia-org');">
+<p><a href="https://donate.oaf.org.au/">
         <img src="<?=IMAGEPATH."donate_greenL.png"?>" width="108" height="43" border="0" align="right" hspace="4"
             vspace="5" alt="Donate"></a>
     <a href="<?php echo $abouturl; ?>" title="link to About Us page">OpenAustralia.org</a> is a
