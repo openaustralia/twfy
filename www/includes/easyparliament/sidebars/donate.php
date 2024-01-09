66baeac986a6515507d01459c6b519775f5a5f12
Matthew Landauer <mlandauer@gmail.com>
2024-01-09 11:10:17 +1100

    OpenAustralia doesn't get any help from volunteers these days. So, change some wording where appropriate.

--- www/includes/easyparliament/sidebars/donate.php
+++ www/includes/easyparliament/sidebars/donate.php
@@ -1,7 +1,7 @@
 <?php
 // This sidebar is on the very front page of the site.
 
-$this->block_start(array('id'=>'help', 'title'=>"Did you know this site is run by a charity and volunteers?"));
+$this->block_start(array('id'=>'help', 'title'=>"Did you know this site is run by a charity?"));
 ?>
 
 <p style="padding-bottom: 30px"><a href="https://donate.oaf.org.au/"><img align="right" src="<?=IMAGEPATH."donate_greenL.png"?>" width="108" height="43" border="0" hspace="4" vspace="5" alt="Donate"></a>
--- www/includes/easyparliament/sidebars/whatisthissite.php
+++ www/includes/easyparliament/sidebars/whatisthissite.php
@@ -15,7 +15,7 @@ $helpurl = $URL->generate();
             vspace="5" alt="Donate"></a>
     <a href="<?php echo $abouturl; ?>" title="link to About Us page">OpenAustralia.org</a> is a
     non-partisan website run by a charity, the <a href="http://www.openaustraliafoundation.org.au">OpenAustralia
-        Foundation</a> and volunteers. It aims to make it easy for people to keep tabs on their
+        Foundation</a>. It aims to make it easy for people to keep tabs on their
     representatives in Parliament.</p>
 <p>The OpenAustralia Foundation is an independent, strictly non-partisan <a
         href="https://www.acnc.gov.au/charity/55c2c06e21ac71e9359a0590b9fc100e">charity</a>, powered by donations from
