467f985b6fae99ff10ee07d0ccc35bc3358365d2
Brenda Wallace <brenda@wallace.net.nz>
2026-01-09 20:30:31 +1300

    the php7 way to connect to the database

--- www/includes/mysql.php
+++ www/includes/mysql.php
@@ -310,7 +310,7 @@ Class MySQL {
 		// These vars come from config.php.
 
 		if (!$global_connection) {
-			$conn = mysql_connect($db_host, $db_user, $db_pass);
+			$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 			if(!$conn) {
 				print ("<p>DB connection attempt failed.</p>");
 				exit;
