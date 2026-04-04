ba814c71a2a73c9b1d4011e5198474e592ed93f1
Brenda Wallace <brenda@wallace.net.nz>
2026-04-04 13:45:31 +1300

    check array before reading

--- www/includes/easyparliament/templates/html/hansard_calendar.php
+++ www/includes/easyparliament/templates/html/hansard_calendar.php
@@ -153,7 +153,7 @@ if (isset($data['years'])) {
                                 if ($recess[0] && $recess[0] != 1)
                                     print ' title="' . $recess[0] . '"';
                                 print '>';
-                            } elseif (isset($recess[0]) && $recess[0]) {
+                            } elseif (count($recess) > 0 && $recess[0]) {
                                 print '<td class="no"';
                                 if ($recess[0] != 1)
                                     print ' title="' . $recess[0] . '"';
