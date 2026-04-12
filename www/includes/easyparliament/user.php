83649961ba0edd97567174f779967a3846ba9155
Brenda Wallace <brenda@wallace.net.nz>
2026-04-12 21:06:00 +1200

    Fix SQL syntax error in user registration query

--- www/includes/easyparliament/user.php
+++ www/includes/easyparliament/user.php
@@ -235,7 +235,7 @@ class USER {
 				registrationip,
 				deleted
 			) VALUES (
-				?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, '0)
+				?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, '0')
 		",
             $details["firstname"],
             $details["lastname"],
