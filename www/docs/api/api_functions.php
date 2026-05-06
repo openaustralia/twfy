0d39e8dee7d7e3346d6a3497f6284042714fcb12
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-05-06 18:57:33 +1000

    Provide implementation for api_functions
    # Conflicts:
    #	www/docs/api/api_functions.php

--- www/docs/api/api_functions.php
+++ www/docs/api/api_functions.php
@@ -4,7 +4,8 @@
  * @file
  */
 
-include_once '../../../../phplib/rabx.php';
+include_once __DIR__ . '/../../../../phplib/rabx.php';
+include_once __DIR__ . '/../../includes/request.php';
 
 // The METHODS.
 
