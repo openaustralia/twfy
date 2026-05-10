4a7c953b806e6e7b470b8d2bb2bf754893c84cde
Brenda Wallace <brenda@wallace.net.nz>
2026-05-10 14:49:29 +1200

    Add alert criteria sanitization and tests

---
+++ www/includes/alertmailer_sanitize.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Remove unknown prefix patterns that make the search layer emit HTML warnings.
+ */
+function sanitize_alert_criteria($criteria) {
+    static $known_prefixes = ['speaker', 'major', 'groupby', 'bias', 'date', 'batch', 'section'];
+
+    return preg_replace_callback(
+        '/\b([A-Za-z][A-Za-z0-9_]*)\:( ?)(?!\/\/)/',
+        function ($matches) use ($known_prefixes) {
+            $prefix = strtolower($matches[1]);
+            if (in_array($prefix, $known_prefixes, TRUE)) {
+                return $matches[0];
+            }
+            return $matches[1] . ' ';
+        },
+        $criteria
+    );
+}
