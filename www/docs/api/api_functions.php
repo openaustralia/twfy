f5f334231af9a9951be2eae319cdf4e92713d7c9
Brenda Wallace <brenda@wallace.net.nz>
2026-05-10 15:16:16 +1200

    remove api_getGeometry

--- www/docs/api/api_functions.php
+++ www/docs/api/api_functions.php
@@ -47,14 +47,6 @@ $methods = [
         'required' => FALSE,
         'help' => 'Returns list of Senators',
     ],
-    /*
-    'getGeometry' => array(
-        'new' => true,
-        'parameters' => array('name'),
-        'required' => false,
-        'help' => 'Returns centre, bounding box of constituencies'
-    ),
-     */
     'getDebates' => [
         'parameters' => ['type', 'date', 'search', 'person', 'gid', 'year', 'order', 'page', 'num'],
         'required' => TRUE,
