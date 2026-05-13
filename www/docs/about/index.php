ec48183488fa359875762469469d887f008399de
Ian Heggie (OAF) <ian@oaf.org.au>
2026-05-14 00:29:21 +1000

    Replace usage of INCLUDESPATH with __DIR__ anchored paths in www/docs/about pages

--- www/docs/about/index.php
+++ www/docs/about/index.php
@@ -6,13 +6,13 @@
 
 $this_page = "about";
 
-include_once "../../includes/easyparliament/init.php";
+include_once __DIR__ . "/../../includes/easyparliament/init.php";
 
 $PAGE->page_start();
 
 $PAGE->stripe_start();
 
-include INCLUDESPATH . 'easyparliament/staticpages/about.php';
+include __DIR__ . '/../../includes/easyparliament/staticpages/about.php';
 
 $PAGE->stripe_end();
 
