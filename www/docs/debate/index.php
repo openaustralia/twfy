8d2fdbb12ce58645b66310bed11cf7bfd8e25aa6
Ian Heggie (OAF) <ian@oaf.org.au>
2026-05-14 00:29:46 +1000

    Replace usage of INCLUDESPATH with __DIR__ anchored paths in www/docs/debate page

--- www/docs/debate/index.php
+++ www/docs/debate/index.php
@@ -4,8 +4,8 @@
  * @file
  */
 
-include_once "../../includes/easyparliament/init.php";
-include_once INCLUDESPATH . "easyparliament/glossary.php";
+include_once __DIR__ . "/../../includes/easyparliament/init.php";
+include_once __DIR__ . "/../../includes/easyparliament/glossary.php";
 
 $this_page = "debate";
 
