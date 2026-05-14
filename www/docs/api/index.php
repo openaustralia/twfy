b72c1ff6b4f5fb012670169352b4098d99a0eed2
Ian Heggie (OAF) <ian@oaf.org.au>
2026-05-14 16:27:02 +1000

    Use https://groups.google.com not http

--- www/docs/api/index.php
+++ www/docs/api/index.php
@@ -243,7 +243,7 @@ function api_front_page($error = '') {
         the language bindings developed for the <a href="http://theyworkforyou.com/api">TheyWorkForYou API</a> won't
         directly work with the OpenAustralia API. If anyone wishes to adapt them or write new bindings, please
         do so, let us know and we'll link to it here. You might want to <a
-            href="http://groups.google.com/group/openaustralia-dev">join the OpenAustralia community mailing list</a>
+            href="https://groups.google.com/group/openaustralia-dev">join the OpenAustralia community mailing list</a>
         to discuss things.</p>
 
     <h3>Examples</h3>
