d84e17c97ae876a5616dd6b609c620a8d31d47e7
Brenda Wallace <brenda@wallace.net.nz>
2026-06-14 15:44:30 +1200

    fix migration

--- db/migrations/20260614000001_add_timestamps_to_models.php
+++ db/migrations/20260614000001_add_timestamps_to_models.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class AddTimestampsToModelsWithoutThem extends AbstractMigration
+final class AddTimestampsToModels extends AbstractMigration
 {
     public function up(): void
     {
