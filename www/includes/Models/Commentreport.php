ccc238de5e4ee18854c923462e78c17d8a4bddd5
Brenda Wallace <brenda@wallace.net.nz>
2026-06-14 14:59:20 +1200

    model for comment report

---
+++ www/includes/Models/Commentreport.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Eloquent model for commentreports table.
+ */
+
+namespace OpenAustralia\TWFY\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Commentreport Eloquent model.
+ *
+ * @property int $report_id
+ * @property int|null $comment_id
+ * @property int|null $user_id
+ * @property string|null $body
+ * @property string|null $reported
+ * @property string|null $resolved
+ * @property int|null $resolvedby
+ * @property string|null $locked
+ * @property int|null $lockedby
+ * @property int $upheld
+ * @property string|null $firstname
+ * @property string|null $lastname
+ * @property string|null $email
+ */
+class Commentreport extends Model {
+
+    protected $table = 'commentreports';
+
+    protected $primaryKey = 'report_id';
+
+    public $timestamps = false;
+}
