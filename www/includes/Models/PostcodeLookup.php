4a8061e6b55a5ecb402bac35b1567a445091fd55
Brenda Wallace <brenda@wallace.net.nz>
2026-06-14 19:02:27 +1200

    feat: add PostcodeLookup model for postcode to constituency mappings

---
+++ www/includes/Models/PostcodeLookup.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OpenAustralia\TWFY\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * PostcodeLookup Eloquent model.
+ *
+ * Maps to the postcode_lookup table containing postcode to constituency mappings.
+ *
+ * @property string $postcode
+ * @property string $name
+ */
+class PostcodeLookup extends Model {
+
+    protected $table = 'postcode_lookup';
+
+    public $timestamps = false;
+
+    public $incrementing = false;
+
+    protected $primaryKey = null;
+
+    protected $fillable = [
+        'postcode',
+        'name',
+    ];
+
+}
