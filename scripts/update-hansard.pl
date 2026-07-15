468b8c642212d64cc764027b8c4471e76fef4371
Brenda Wallace <brenda@wallace.net.nz>
2026-07-16 09:11:33 +1200

    Enhance comments and SQL syntax in update-hansard.pl for clarity

--- scripts/update-hansard.pl
+++ scripts/update-hansard.pl
@@ -1,5 +1,8 @@
 #! /usr/bin/perl
 # vim:sw=8:ts=8:et:nowrap
+
+# Updates Hansard timing fields for newly processed records in the database.
+
 use strict;
 
 use FindBin;
@@ -10,12 +13,12 @@ use lib "$FindBin::Bin/../../perllib";
 use mySociety::Config;
 mySociety::Config::set_file('../conf/general');
 
-use DBI; 
+use DBI;
 
 my $dsn = 'DBI:mysql:database=' . mySociety::Config::get('DB_NAME'). ':host=' . mySociety::Config::get('DB_HOST');
 my $dbh = DBI->connect($dsn, mySociety::Config::get('DB_USER'), mySociety::Config::get('DB_PASSWORD'), { RaiseError => 1, PrintError => 0 });
 
-my $sth = $dbh->prepare("update hansard set htime=? where gid = ?");
+my $sth = $dbh->prepare("UPDATE hansard SET htime=? WHERE gid = ?");
 while (<>) {
         next if /^--/;
         my ($gid, $time) = split /\t/;
